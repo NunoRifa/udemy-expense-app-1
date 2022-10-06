37c41785d9f42f2e746940089d4e320c01d5d0b9
Nuno Rifa <nuno180202@gmail.com>
2022-10-06 23:28:42 +0700

    Add component ExpenseList

---
+++ src/components/Expenses/ExpenseList.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import "./ExpenseList.css";
+
+import ExpenseItem from "./ExpenseItem";
+
+const ExpenseList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no Expenses.</h2>;
+  }
+
+  if (props.items.length > 0) {
+    props.items.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpenseList;
